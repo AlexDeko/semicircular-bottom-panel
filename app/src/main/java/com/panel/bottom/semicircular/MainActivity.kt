e3596bfa772425c8b4d04b515f0be4b4d2b21030
Alexandr Deynenko <alex.deko.69@gmail.com>
2021-08-04 11:28:29 +0300

    Initial commit

---
+++ app/src/main/java/com/panel/bottom/semicircular/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.panel.bottom.semicircular
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
